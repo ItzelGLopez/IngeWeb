47113f6e81f7a14db92dd3a1501e9a7f6ff1d40e
SajitVentura <elopezv1700@alumno.ipn.mx>
2024-01-16 16:27:02 -0600

    Proyecto

--- Proyecto2/Proyecto2/src/routes/index.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const router = Router();
-
-router.get("/",(req, res)=>{  
-    res.render('index', {titulo: 'Aplicación web con express y HBS'})  
-});
-
-export default router; 

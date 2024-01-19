fd129fb445beb1abbf0d78caf59b4f924b86bea9
Itzel G. López <lopez.cedillo.itzel@gmail.com>
2024-01-18 18:43:25 -0600

    Update db.js
    cambio de contraseña de la base de datos

--- Proyecto/APIREST/db.js
+++ Proyecto/APIREST/db.js
@@ -4,7 +4,7 @@ import {createPool} from 'mysql2/promise';
 export const conn = createPool({
   host: 'localhost',
   user: 'root',
-  password: '123456',
+  password: '85060434Yo',
   port: 3306,
   database: 'Proyecto'
 });
